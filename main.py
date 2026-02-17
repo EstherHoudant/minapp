38b56c29d6bca74f18395f02e54e0ee58d3ea286
Gabriel Jeantet <gabriel.jeantetpro@gmail.com>
2026-02-17 10:49:28 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import arel
+import os
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
+os.environ["DEBUG"] = "true"
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+if _debug := os.environ.get("DEBUG"):
+    hot_reload = arel.HotReload(paths=[arel.Path(".")])
+    app.add_websocket_route("/hot-reload", route=hot_reload, name="hot-reload")
+    app.add_event_handler("startup", hot_reload.startup)
+    app.add_event_handler("shutdown", hot_reload.shutdown)
+    templates.env.globals["DEBUG"] = _debug
+    templates.env.globals["hot_reload"] = hot_reload
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(
+        request = request , name="index.html"
+    )
